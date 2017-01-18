1dc49182b5f6cbe21b0a4d01188067324a18adb0
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-01-18 22:06:56 +0200

    Tsugi files

---
+++ vendor/tsugi/lib/tests/UI/OutputTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once "src/UI/Output.php";
+
+use \Tsugi\UI\Output;
+
+class OutputTest extends PHPUnit_Framework_TestCase
+{
+    public function testConstruct() {
+        $OUTPUT = new Output();
+    }
+
+
+}
